6614f2a9b7ebe44d21068bc22eabcc1f3065ab0c
Douglas Sales <doug.sales23@gmail.com>
2020-08-21 02:52:04 -0300

    Add features required by tests.

---
+++ src/database/migrations/1597898972301-AddCategorieIdToTransactions.ts
@@ -0,0 +1,37 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AddCategorieIdToTransactions1597898972301
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'transactions',
+      new TableColumn({
+        name: 'category_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        name: 'fk_transaction_category',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'fk_transaction_category');
+    await queryRunner.dropColumn('transactions', 'categori_id');
+  }
+}
